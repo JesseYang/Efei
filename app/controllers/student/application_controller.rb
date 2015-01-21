db5386e5374e013e8986dd9276a1ed307aec9f1c
JesseYang <jesse.yang1985@gmail.com>
2015-01-21 10:21:40 +0800

    student session

--- app/controllers/student/application_controller.rb
+++ app/controllers/student/application_controller.rb
@@ -1,5 +1,10 @@
 class Student::ApplicationController < ApplicationController
   layout 'layouts/student'
+  before_filter :student_init
+
+  def student_init
+    refresh_session(params[:auth_key])
+  end
 
   def require_student
     if current_user.blank?
