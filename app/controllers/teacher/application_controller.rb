95ce472d902a96dad5fc83925e9f05be3b303c4a
JesseYang <jesse.yang1985@gmail.com>
2015-01-24 19:36:21 +0800

    compose homework

--- app/controllers/teacher/application_controller.rb
+++ app/controllers/teacher/application_controller.rb
@@ -4,8 +4,8 @@ class Teacher::ApplicationController < ApplicationController
   before_filter :require_teacher, :init_teacher
 
   def init_teacher
-    @compose = current_user.compose
-    @compose_qid_str = @compose.questions.map { |e| e.id.to_s }.join(',')
+    @compose = current_user.ensure_compose
+    @compose_qid_str = ( @compose.questions || [] ).map { |e| e.id.to_s }.join(',')
     @show_compose = @compose.homework.present?
   end
 end
