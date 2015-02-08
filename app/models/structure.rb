dc1ab5936dff74e667f0e9683d58649749b913da
JesseYang <jesse.yang1985@gmail.com>
2015-02-08 08:55:49 +0800

    construct structure

--- app/models/structure.rb
+++ app/models/structure.rb
@@ -6,34 +6,56 @@ class Structure
   field :subject, type: Integer
   # edition, book, chapter, section, subsection, point
   field :type, type: String
+  field :children_id, type: Array, default: []
 
   has_and_belongs_to_many :children, class_name: "Structure", inverse_of: :parents
   has_and_belongs_to_many :parents, class_name: "Structure", inverse_of: :children
 
-  def self.import_edition
-    self.destroy_all
+  def self.import
+    cur_edition = nil
+    cur_book = nil
+    cur_chapter = nil
+    cur_section = nil
+    f = File.open('public/structure')
+    ary = f.read.split("\n")
+    ary.each do |ele|
+      n = ele.index /\S/
+      case n
+      when 0
+        s = Structure.create(name: ele.strip, subject: 2, type: "edition")
+        cur_edition = s
+      when 1
+        s = Structure.create(name: ele.strip, subject: 2, type: "book")
+        cur_book = s
+        cur_edition.children << s
+        cur_edition.children_id << s.id.to_s
+        cur_edition.save
+      when 2
+        s = Structure.create(name: ele.strip, subject: 2, type: "chapter")
+        cur_chapter = s
+        cur_book.children << s
+        cur_book.children_id << s.id.to_s
+        cur_book.save
+      when 3
+        s = Structure.create(name: ele.strip, subject: 2, type: "section")
+        cur_section = s
+        cur_chapter.children << s
+        cur_chapter.children_id << s.id.to_s
+        cur_chapter.save
+      when 4
+        s = Structure.create(name: ele.strip, subject: 2, type: "subsection")
+        cur_section.children << s
+        cur_section.children_id << s.id.to_s
+        cur_section.save
+      end
+    end
+  end
 
-    # editions
-    editions = []
-    editions << self.create(name: "人教A版", subject: 2, type: "edition")
-    editions << self.create(name: "人教B版", subject: 2, type: "edition")
-    editions << self.create(name: "北师大", subject: 2, type: "edition")
-    editions << self.create(name: "苏教版", subject: 2, type: "edition")
-    editions << self.create(name: "湘教版", subject: 2, type: "edition")
-
-    # text books
-    t = editions[0].children.create(name: "必修1", subject: 2, type: "book")
-    t.children.create(name: "集合与函数概念", subject: 2, type: "chapter")
-    editions[0].children.create(name: "必修2", subject: 2, type: "book")
-    editions[0].children.create(name: "必修3", subject: 2, type: "book")
-    editions[0].children.create(name: "必修4", subject: 2, type: "book")
-    editions[0].children.create(name: "必修5", subject: 2, type: "book")
-    editions[0].children.create(name: "选修1-1", subject: 2, type: "book")
-    editions[0].children.create(name: "选修1-2", subject: 2, type: "book")
-    editions[0].children.create(name: "选修2-1", subject: 2, type: "book")
-    editions[0].children.create(name: "选修2-2", subject: 2, type: "book")
-    editions[0].children.create(name: "选修2-3", subject: 2, type: "book")
-    editions[0].children.create(name: "选修4-1", subject: 2, type: "book")
-    editions[0].children.create(name: "选修4-4", subject: 2, type: "book")
+  def print(indent)
+    puts indent + self.name
+    self.children_id.each do |eid|
+      e = Structure.find(eid)
+      e.print(indent + " ")
+    end
   end
 end
