a2d691465a6a24673d990bb1a973710756881f3d
JesseYang <jesse.yang1985@gmail.com>
2015-03-15 09:46:49 +0800

    update

--- app/controllers/teacher/papers_controller.rb
+++ app/controllers/teacher/papers_controller.rb
@@ -15,6 +15,15 @@ class Teacher::PapersController < Teacher::ApplicationController
 
   def list
   	@papers = Homework.where(type: "paper", finished: true)
+    if params[:paper_year] != "0" && params[:paper_year] != ""
+      @papers = @papers.where(year: params[:paper_year].to_i)
+    end
+    if params[:paper_province] != "0" && params[:paper_province] != ""
+      @papers = @papers.where(province: params[:paper_province])
+    end
+    if params[:paper_type] != "0" && params[:paper_type] != ""
+      @papers = @papers.where(paper_type: params[:paper_year])
+    end
   	@papers = auto_paginate_ajax(@papers, params[:page] || 1, params[:per_page] || 10)
   	render_json({ papers: @papers }) and return
   end
