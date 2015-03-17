86918fc8557951428677bb048fb536ab07b9f486
JesseYang <jesse.yang1985@gmail.com>
2015-03-17 21:00:33 +0800

    updat

--- app/controllers/qrcodes_controller.rb
+++ app/controllers/qrcodes_controller.rb
@@ -1,10 +1,10 @@
 require 'rqrcode_png'
 class QrcodesController < ApplicationController
   def index
-    qr = RQRCode::QRCode.new("#{Rails.application.config.server_host}/~#{params[:link]}", :size => 4, :level => :h )
+    qr = RQRCode::QRCode.new("#{Rails.application.config.server_host}/~#{params[:link]}", :size => 3, :level => :q )
     png = qr.to_img
     temp_img_name = "public/qr_code/#{params[:link]}.png"
-    png.resize(80, 80).save(temp_img_name)
+    png.resize(60, 60).save(temp_img_name)
     # render :text => "/qr_code/#{params[:link]}.png" and return
     send_file "public/qr_code/#{params[:link]}.png", type: 'image/png'
   end
