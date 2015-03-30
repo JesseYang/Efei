ee85ef8061cef6390195993bb4b3cc60a3757074
JesseYang <jesse.yang1985@gmail.com>
2015-03-30 16:48:15 +0800

    bigger qr code

--- app/controllers/qrcodes_controller.rb
+++ app/controllers/qrcodes_controller.rb
@@ -10,18 +10,20 @@ class QrcodesController < ApplicationController
   end
 
   def student_android_app_qr_code
+    size = params[:size].present? ? params[:size].to_i : 100
     qr = RQRCode::QRCode.new("#{Rails.application.config.student_android_app_url}", :size => 5, :level => :h )
     png = qr.to_img
-    temp_img_name = "public/qr_code/student_android_app_url.png"
-    png.resize(100, 100).save(temp_img_name)
-    send_file "public/qr_code/student_android_app_url.png", type: 'image/png'
+    temp_img_name = "public/qr_code/student_android_app_url-#{size}.png"
+    png.resize(size, size).save(temp_img_name)
+    send_file temp_img_name, type: 'image/png'
   end
 
   def student_ios_app_qr_code
+    size = params[:size].present? ? params[:size].to_i : 100
     qr = RQRCode::QRCode.new("#{Rails.application.config.student_ios_app_url}", :size => 5, :level => :h )
     png = qr.to_img
-    temp_img_name = "public/qr_code/student_ios_app_url.png"
-    png.resize(100, 100).save(temp_img_name)
-    send_file "public/qr_code/student_ios_app_url.png", type: 'image/png'
+    temp_img_name = "public/qr_code/student_ios_app_url-#{size}.png"
+    png.resize(size, size).save(temp_img_name)
+    send_file temp_img_name, type: 'image/png'
   end
 end
