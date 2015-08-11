d61e304f593c6485413cca105520f1b26d5ecd62
JesseYang <jesse.yang1985@gmail.com>
2015-08-11 15:01:26 +0800

    u

--- app/controllers/tablet/videos_controller.rb
+++ app/controllers/tablet/videos_controller.rb
@@ -3,7 +3,7 @@ class Tablet::VideosController < Tablet::ApplicationController
 
   def index
     lesson = Lesson.find(params[:lesson_id])
-    videos = lesson.video_id_ary.map { |e| Video.find(e) } .each_with_index.map do |v, i|
+    videos = lesson.video_id_ary.select { |e| e.present? } .map { |e| Video.find(e) } .each_with_index.map do |v, i|
       v.info_for_tablet(lesson, i)
     end
     render_with_auth_key({videos: videos}) and return
