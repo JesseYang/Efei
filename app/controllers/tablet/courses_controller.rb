d2f2b27b5ab9b82d4e27df48c92236cb24836949
JesseYang <jesse.yang1985@gmail.com>
2015-08-15 10:24:33 +0800

    u

--- app/controllers/tablet/courses_controller.rb
+++ app/controllers/tablet/courses_controller.rb
@@ -8,6 +8,11 @@ class Tablet::CoursesController < Tablet::ApplicationController
     render_with_auth_key({courses: courses}) and return
   end
 
+  def show
+  	c = Course.where(id: params[:id]).first
+  	render_with_auth_key({course: c.info_for_tablet}) and return
+  end
+
   def create
   	
   end
