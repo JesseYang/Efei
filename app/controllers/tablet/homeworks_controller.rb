6205a35bdbac8a1545d4bab6c5a2ceedd577dc7f
JesseYang <jesse.yang1985@gmail.com>
2015-09-19 19:31:45 +0800

    u

--- app/controllers/tablet/homeworks_controller.rb
+++ app/controllers/tablet/homeworks_controller.rb
@@ -6,9 +6,9 @@ class Tablet::HomeworksController < Tablet::ApplicationController
     post_test = Homework.where(lesson_post_test_id: params[:lesson_id]).first
     exercise = Homework.where(lesson_exercise_id: params[:lesson_id]).first
     homeworks = [ ]
-    homeworks << pre_test.info_for_tablet if pre_test.present?
-    homeworks << post_test.info_for_tablet if post_test.present?
-    homeworks << exercise.info_for_tablet if exercise.present?
+    homeworks << pre_test.info_for_tablet("pre_test") if pre_test.present?
+    homeworks << post_test.info_for_tablet("post_test") if post_test.present?
+    homeworks << exercise.info_for_tablet("exercise") if exercise.present?
     render_with_auth_key({homeworks: homeworks}) and return
   end
 end
