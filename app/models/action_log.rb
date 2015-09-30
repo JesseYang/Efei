2da258183411f13a8d3ab7e5a141f422b5383096
JesseYang <jesse.yang1985@gmail.com>
2015-09-30 19:37:55 +0800

    u

--- app/models/action_log.rb
+++ app/models/action_log.rb
@@ -21,7 +21,17 @@ class ActionLog
       id_ary << l.delete("id")
       auth_key = l.delete("auth_key")
       l["student_id"] = User.find_by_auth_key(auth_key).id.to_s
-      ActionLog.create(l)
+      ActionLog.create({
+        happen_at: l["happen_at"],
+        action: l["action"],
+        video_id_1: l["video_id_1"],
+        video_id_2: l["video_id_2"],
+        video_time_1: l["video_time_1"],
+        video_time_2: l["video_time_2"],
+        question_id: l["question_id"],
+        snapshot_id: l["snapshot_id"],
+        student_id: l["student_id"]
+      })
     end
     id_ary
   end
