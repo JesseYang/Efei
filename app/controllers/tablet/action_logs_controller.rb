9d3b7e9bebe248665e8a66cf655bb2f1db000dfa
JesseYang <jesse.yang1985@gmail.com>
2015-10-02 20:32:48 +0800

    u

--- app/controllers/tablet/action_logs_controller.rb
+++ app/controllers/tablet/action_logs_controller.rb
@@ -1,6 +1,10 @@
 # encoding: utf-8
 class Tablet::ActionLogsController < Tablet::ApplicationController
 
+  def index
+    @logs = ActionLog.all.asc(:happen_at)
+  end
+
   def create
     retval = ActionLog.batch_create(params[:logs])
     render json: { success: true, max_id: retval.max } and return
--- app/models/action_log.rb
+++ app/models/action_log.rb
@@ -3,8 +3,10 @@ class ActionLog
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  field :device_id, type: String
+  field :log_id, type: Integer
   field :happen_at, type: Integer
-  field :action, type: String
+  field :action, type: Integer
   field :video_id_1, type: String
   field :video_id_2, type: String
   field :video_time_1, type: Integer
@@ -51,7 +53,12 @@ class ActionLog
       id_ary << l.delete("id")
       auth_key = l.delete("auth_key")
       l["student_id"] = User.find_by_auth_key(auth_key).id.to_s
+      if ActionLog.where(device_id: l["device_id"], log_id: l["log_id"]).first.present?
+        next
+      end
       ActionLog.create({
+        device_id: l["device_id"],
+        log_id: l["log_id"],
         happen_at: l["happen_at"],
         action: l["action"],
         video_id_1: l["video_id_1"],
@@ -64,5 +71,50 @@ class ActionLog
       })
     end
     id_ary
+  end
+
+  def self.action_desc(action)
+    case action
+    when 0
+      return "进入课程"
+    when 1
+      return "进入课前例题"
+    when 2
+      return "进入课前例题总结"
+    when 3
+      return "进入视频"
+    when 4
+      return "暂停视频"
+    when 5
+      return "播放视频"
+    when 6
+      return "切换视频"
+    when 7
+      return "进入练习"
+    when 8
+      return "从练习返回"
+    when 9
+      return "开始快进"
+    when 10
+      return "停止快进"
+    when 11
+      return "开始快退"
+    when 12
+      return "停止快退"
+    when 13
+      return "进入截图"
+    when 14
+      return "从截图返回"
+    when 15
+      return "进入课后测试"
+    when 16
+      return "进入课后测试总结"
+    when 17
+      return "进入课后测试题目讲解"
+    when 18
+      return "返回课后测试总结"
+    when 19
+      return "离开课程"
+    end
   end
 end
