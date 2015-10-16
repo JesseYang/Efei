c854dc1d951c33f91918e7e8352d2243a9ac9219
JesseYang <jesse.yang1985@gmail.com>
2015-10-16 14:23:29 +0800

    u

--- lib/integer.rb
+++ lib/integer.rb
@@ -3,6 +3,35 @@ class Integer
     return ("A".."Z").to_a[self]
   end
 
+  def to_abcd
+    case self
+    when 0
+      "D"
+    when 1
+      "D+"
+    when 2
+      "C-"
+    when 3
+      "C"
+    when 4
+      "C+"
+    when 5
+      "B-"
+    when 6
+      "B"
+    when 7
+      "B+"
+    when 8
+      "A-"
+    when 9
+      "A"
+    when 10
+      "A+"
+    else
+      ""
+    end
+  end
+
   def to_time
     time = self
     str = ""
